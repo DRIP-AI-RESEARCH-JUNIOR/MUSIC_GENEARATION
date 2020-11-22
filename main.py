5eb3ee4ccfd684b70e6c25c464697fbc64cfeb03
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2020-11-23 01:01:07 +0530

    training updated

--- main.py
+++ main.py
@@ -11,3 +11,132 @@ import matplotlib.pyplot as plt
 from model import *
 from ops import *
 
+def load_data():
+  # TO do -: write this
+  return train_loader, val_loader
+
+def train(netG, netD, optG, optD, batch_size, nz, device=torch.device('cuda')):
+  
+  fixed_noise = torch.randn(batch_size, nz, device=device)
+  netD.train()
+  netG.train()
+  
+  real_label = 1
+  fake_label = 0
+  average_lossD = 0
+  average_lossG = 0
+  average_D_x   = 0
+  average_D_G_z = 0
+
+  lossD_list =  []
+  lossD_list_all = []
+  lossG_list =  []
+  lossG_list_all = []
+  D_x_list = []
+  D_G_z_list = []
+  
+  for epoch in range(epochs):
+    sum_lossD = 0
+    sum_lossG = 0
+    sum_D_x   = 0
+    sum_D_G_z = 0
+    
+    for i, (data,prev_data,chord) in enumerate(train_loader, 0):
+      
+      #############################################################
+      # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+      #############################################################
+      
+      # train with real      
+      netD.zero_grad()
+      real_cpu = data.to(device)
+      prev_data_cpu = prev_data.to(device)
+      chord_cpu = chord.to(device)
+      
+      batch_size = real_cpu.size(0)
+      label = torch.full((batch_size,), real_label, device=device)
+      D, D_logits, fm = netD(real_cpu,chord_cpu,batch_size,pitch_range)
+      
+      #####loss
+      d_loss_real = reduce_mean(sigmoid_cross_entropy_with_logits(D_logits, 0.9*torch.ones_like(D)))
+      d_loss_real.backward(retain_graph=True)
+      D_x = D.mean().item()
+      sum_D_x += D_x 
+      
+      # train with fake
+      noise = torch.randn(batch_size, nz, device=device)
+      fake = netG(noise,prev_data_cpu,chord_cpu,batch_size,pitch_range)
+      label.fill_(fake_label)
+      D_, D_logits_, fm_ = netD(fake.detach(),chord_cpu,batch_size,pitch_range)
+      d_loss_fake = reduce_mean(sigmoid_cross_entropy_with_logits(D_logits_, torch.zeros_like(D_)))
+      
+      d_loss_fake.backward(retain_graph=True)
+      D_G_z1 = D_.mean().item()
+      errD = d_loss_real + d_loss_fake
+      errD = errD.item()
+      lossD_list_all.append(errD)
+      sum_lossD += errD
+      optimizerD.step()
+      
+      #############################################
+      # (2) Update G network: maximize log(D(G(z)))
+      #############################################
+      
+      netG.zero_grad()
+      label.fill_(real_label)  # fake labels are real for generator cost
+      D_, D_logits_, fm_= netD(fake,chord_cpu,batch_size,pitch_range)
+      
+      ###loss
+      g_loss0 = reduce_mean(sigmoid_cross_entropy_with_logits(D_logits_, torch.ones_like(D_)))
+      #Feature Matching
+      features_from_g = reduce_mean_0(fm_)
+      features_from_i = reduce_mean_0(fm)
+      fm_g_loss1 =torch.mul(l2_loss(features_from_g, features_from_i), 0.1)
+
+      mean_image_from_g = reduce_mean_0(fake)
+      smean_image_from_i = reduce_mean_0(real_cpu)
+      fm_g_loss2 = torch.mul(l2_loss(mean_image_from_g, smean_image_from_i), 0.01)
+
+      errG = g_loss0 + fm_g_loss1 + fm_g_loss2
+      errG.backward(retain_graph=True)
+      D_G_z2 = D_.mean().item()
+      optimizerG.step()
+      
+      ###################################################
+      # (3) Update G network again: maximize log(D(G(z)))
+      ###################################################
+      
+      netG.zero_grad()
+      label.fill_(real_label)  # fake labels are real for generator cost
+      D_, D_logits_, fm_ = netD(fake,chord_cpu,batch_size,pitch_range)
+
+      ###loss
+      g_loss0 = reduce_mean(sigmoid_cross_entropy_with_logits(D_logits_, torch.ones_like(D_)))
+      #Feature Matching
+      features_from_g = reduce_mean_0(fm_)
+      features_from_i = reduce_mean_0(fm)
+      loss_ = nn.MSELoss(reduction='sum')
+      feature_l2_loss = loss_(features_from_g, features_from_i)/2
+      fm_g_loss1 =torch.mul(feature_l2_loss, 0.1)
+
+      mean_image_from_g = reduce_mean_0(fake)
+      smean_image_from_i = reduce_mean_0(real_cpu)
+      mean_l2_loss = loss_(mean_image_from_g, smean_image_from_i)/2
+      fm_g_loss2 = torch.mul(mean_l2_loss, 0.01)
+      errG = g_loss0 + fm_g_loss1 + fm_g_loss2
+      sum_lossG +=errG
+      errG.backward()
+      lossG_list_all.append(errG.item())
+
+      D_G_z2 = D_.mean().item()
+      sum_D_G_z += D_G_z2
+      optimizerG.step()
+      
+      if i % 100 == 0:
+        vutils.save_image(real_cpu, '%s/real_samples.png' % 'file', normalize=True)
+        fake = netG(fixed_noise,prev_data_cpu,chord_cpu,batch_size,pitch_range)
+        vutils.save_image(fake.detach(), '%s/fake_samples_epoch_%03d.png' % ('file', epoch), normalize=True)
+  
+  if epoch % 5 == 0:
+    print('[{}/{}][{}/{}] Loss_D: {:.4f} Loss_G: {:.4f} D(x): {:.4f} D(G(z)): {:.4f} / {:.4f}'.
+          format(epoch, epochs, i, len(train_loader),errD, errG, D_x, D_G_z1, D_G_z2))
